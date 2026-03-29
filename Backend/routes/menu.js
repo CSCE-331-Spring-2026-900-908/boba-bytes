3d0933431acfb0dd666ec3d6433f04b26cef85bb
Roa Elsaigh <roa.elsaigh@tamu.edu>
2026-03-29 18:14:48 -0500

    chnages for customer kiosk insterface

--- Backend/routes/menu.js
+++ Backend/routes/menu.js
@@ -1,11 +1,26 @@
 import express from "express";
-import { pool } from "../db/pool.js";
+import pool from "../db/pool.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/items", async (req, res) => {
+  try {
     const result = await pool.query("SELECT * FROM menu_items");
     res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
+router.get("/categories", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT DISTINCT category FROM menu_items");
+    const categories = result.rows.map(r => r.category);
+    res.json(categories);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
 });
 
 export default router;
