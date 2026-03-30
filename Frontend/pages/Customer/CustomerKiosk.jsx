a316a439dd034cacdb4877e832422215bce92eeb
Deep10sun <Deep10sun@gmail.com>
2026-03-29 23:53:35 -0500

    modified API_BASE for render

--- Frontend/pages/Customer/CustomerKiosk.jsx
+++ Frontend/pages/Customer/CustomerKiosk.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './CustomerKiosk.css';
 
-const API_BASE = 'https://boba-bytes.onrender.com';
+const API_BASE = 'https://boba-bytes.onrender.com/api';
 
 function CustomerKiosk() {
   const [menuItems, setMenuItems] = useState([]);
